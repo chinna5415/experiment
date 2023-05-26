9d2f966cfdd97383f1870ff7d18b84e309792386
manikantahima <manikanta.hima@moneyview.in>
2023-05-26 14:38:19 +0530

    s

--- src/test/java/android/Androids.java
+++ src/test/java/android/Androids.java
@@ -1,19 +1,38 @@
 package android;
 
-import android.Drivers.Drivers;
-import android.Drivers.DriversFactory;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Androids {
 
     @Test
     public void temp() throws MalformedURLException {
-        Drivers driver = new DriversFactory().getDriver("android");
-        driver.initializeDriver();
-        AndroidDriver androidDriver = (AndroidDriver) driver.getDriver();
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), getDesiredCapabilities());;
+    }
+
+    private DesiredCapabilities getDesiredCapabilities() {
+
+        Device deviceConfig = ConfigFactory.create(Device.class);
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities() {{
+
+            setCapability(MobileCapabilityType.DEVICE_NAME, deviceConfig.device_name());
+            setCapability(MobileCapabilityType.AUTOMATION_NAME, deviceConfig.automation_name());
+            setCapability(MobileCapabilityType.PLATFORM_NAME, deviceConfig.platform_name());
+            setCapability(MobileCapabilityType.PLATFORM_VERSION, deviceConfig.platform_version());
+            setCapability("appPackage", deviceConfig.app_package());
+            setCapability("appActivity", deviceConfig.app_activity());
+            setCapability(MobileCapabilityType.UDID, deviceConfig.uuid());
+
+        }};
+
+        return desiredCapabilities;
     }
 
 }
