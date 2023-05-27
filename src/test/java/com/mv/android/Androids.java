29c516b74dc044d15c0463523c3f967ff56394d6
manikantahima <manikanta.hima@moneyview.in>
2023-05-27 22:21:50 +0530

    script changes

--- src/test/java/com/mv/android/Androids.java
+++ src/test/java/com/mv/android/Androids.java
@@ -3,6 +3,8 @@ package com.mv.android;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -12,8 +14,15 @@ import java.net.URL;
 public class Androids {
 
     @Test
-    public void temp() throws MalformedURLException {
-        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), getDesiredCapabilities());;
+    public void temp() throws MalformedURLException, InterruptedException {
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), getDesiredCapabilities());
+
+        WebElement webElement = driver.findElement(By.xpath("//android.widget.Button[@text='Accept & continue']"));
+        webElement.click();
+
+        Thread.sleep(100);
+        webElement = driver.findElement(By.xpath("//android.widget.Button[@text='No thanks']"));
+        webElement.click();
     }
 
     private DesiredCapabilities getDesiredCapabilities() {
